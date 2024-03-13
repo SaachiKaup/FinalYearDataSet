946e1fb999d14b6cc1a9682cde428aaa40dca25e
Saachi Kaup <saachi.kaup@gmail.com>
2024-03-13 14:19:31 +0530

    yield rows from 2000 to 4000

---
+++ split_csv.py
@@ -0,0 +1,26 @@
+import csv
+from itertools import islice
+
+filename = 'train.csv'
+
+csv.field_size_limit(1000000000)
+
+def fix_nulls(s):
+    for line in s:
+        yield line.replace('\0', ' ')
+
+def lazy_print(s):
+    for line in s:
+        yield line
+
+def yield_from_index(csv_reader):
+    for row in csv_reader:
+        yield row
+
+with open(filename, 'r+') as f:
+    reader = csv.reader(fix_nulls(f))
+    #print(next(lazy_print(f)))
+    for item in islice(yield_from_index(reader), 2002, 4000):
+        print(len(item))
+    #print(next(yield_from_index(reader)))
+
